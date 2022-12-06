4ac0525f803a6a7f7cd623314df930cfd91bc4be
kayhar1 <tilekovakb@gmail.com>
2022-12-06 19:30:49 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
